a9f3668a31d6a163879322f3449ecf5dc6fe31c8
AMutaa <piusandru@gmail.com>
2018-08-22 18:18:21 -0400

    fetch api data

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 
 
 class Home extends Component {
+  state = {
+    allBikes: [],
+  }
 
+  async componentDidMount() {
+    try {
+      const results = await fetch('http://api.citybik.es/v2/networks');
+      const allBikes = await results.json();
+      const usaBikes = allBikes.networks.filter((item) => item.location.country === 'US')
+
+      this.setState({
+        allBikes: usaBikes
+      })
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
   render() {
     return (
       <div>
