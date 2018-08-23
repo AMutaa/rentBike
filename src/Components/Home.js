0b9b262156260dc3f9d437243f8eeaad38441daa
AMutaa <piusandru@gmail.com>
2018-08-23 15:39:24 -0400

    api pull cities

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,46 +4,29 @@ import React, { Component } from 'react';
 class Home extends Component {
   state = {
     allBikes: [],
-    name: ''
   }
 
   async componentDidMount() {
     try {
+      const results = await fetch('http://api.citybik.es/v2/networks');
+      const allBikes = await results.json()
+      const usaBikes = allBikes.networks.filter((item) => item.location.country === 'US');
+      console.log(usaBikes);
 
-
-
-      const results = await fetch('http://api.citybik.es/v2/networks/hubway');
-
-      const allBikes = await results.json();
-
-      console.log(allBikes.network.name);
-      // const usaBikes = allBikes.networks.filter((item) => item.location.country === 'US')
-      // console.log(allBikes.network)
-      // console.log(allBikes.network.name)
-      // console.log(allBikes.network.stations)
-
-      setTimeout(() => {
-        this.setState({
-          // allBikes: allBikes
-          name: allBikes.network.name
-        })
-      }, 10000);
-
-
+      this.setState({
+        allBikes: usaBikes
+      })
 
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
   render() {
-
-
-
     return (
       <div>
-        <h1>{this.state.name}</h1>
-        {/* {this.state.allBikes} */}
-        {/* {this.state.allBikes.map((item) => <li key={item.id}>{item.location.city}</li>)} */}
+        {this.state.allBikes.map((item, index) =>
+          <li key={index}>{item.location.city}</li>
+        )}
       </div>
     )
   }
