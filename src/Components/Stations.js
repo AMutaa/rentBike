779d7b30e8c2b5ac032f6e510d03529a79c8eb66
AMutaa <piusandru@gmail.com>
2018-10-25 18:01:24 -0400

    update

--- src/Components/Stations.js
+++ src/Components/Stations.js
@@ -5,7 +5,6 @@ const Stations = ({ stations }) => (
 
   <div>
     <ul className="station_info">{stations.map((station, index) => <Station key={index} station={station} />)}</ul>
-
   </div>
 )
 export default Stations
