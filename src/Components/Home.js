701cf8d32fd986a97c6b1811c200b26f3839a43f
AMutaa <piusandru@gmail.com>
2019-02-28 23:27:28 -0500

    restructure files

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import City from './City';
-import FilterBox from './FilterBox';
+import styled from 'styled-components'
 
 
 class Home extends Component {
   state = {
-    usaCities: [],
+    searchInput: '',
+    usaCities: []
+  }
 
+  handleChange = (e) => {
+    const typedChar = e.target.value
+    console.log(typedChar)
+    this.setState({
+      searchInput: typedChar
+    })
   }
 
   async componentDidMount() {
@@ -26,11 +34,11 @@ class Home extends Component {
   }
   render() {
 
-    const { usaCities } = this.state;
+    const { usaCities, searchInput } = this.state;
 
     return (
       <div>
-        <FilterBox />
+        <FilterBox onChange={this.handleChange} value={searchInput} />
         <h2 className="select">Select your City</h2>
         <div className="cities">
           {usaCities.map((city, index) => <City key={index} city={city} />)}
@@ -41,3 +49,14 @@ class Home extends Component {
 }
 
 export default Home;
+
+
+const FilterBox = styled.input`
+width: 300px;
+height: 50px;
+border: 2px solid #FFBE06;
+outline:none;
+border-radius: 4px;
+margin: 20px 0;
+font-size: 20px;
+`
