43f524cfdebe0df31dd9092eba6da9c0bbd1153f
AMutaa <piusandru@gmail.com>
2019-05-20 23:19:27 -0400

    sinon, lifecycle test

--- src/Components/__tests__/Home.test.js
+++ src/Components/__tests__/Home.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Home from '../Home';
+import sinon from 'sinon'
 
 describe('Tests Home Component', () => {
   it('renders', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.exists()).toBe(true)
+  })
+
+  test('should call ComponentDidMount', () => {
+    const spy = sinon.spy(Home.prototype, 'componentDidMount');
+    const wrapper = mount(<Home />)
+    expect(spy.calledOnce).toBeTruthy();
   })
 })
