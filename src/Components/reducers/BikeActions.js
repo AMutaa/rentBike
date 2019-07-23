df98be6bebcdae2b10489e52d9ad5de4a2a2160b
AMutaa <piusandru@gmail.com>
2019-07-22 20:26:16 -0400

    update actions

--- src/Components/reducers/BikeActions.js
+++ src/Components/reducers/BikeActions.js
@@ -1,5 +1,5 @@
 export const GET_CITIES = "GET_CITIES";
-export const GET_STATIONS = "GET_STATIONS";
+export const CITY_DETAILS = "GET_DETAILS";
 
 export function getCities() {
   return async function(dispatch) {
@@ -15,10 +15,17 @@ export function getCities() {
   };
 }
 
-export function getStations(id) {
+export function getCityDetails(id) {
   return async function(dispatch) {
-    const results = await fetch(
-      `https://api.citybik.es/v2/networks/${this.props.match.params.id}`
-    );
+    const results = await fetch(`https://api.citybik.es/v2/networks/${id}`);
+    const details = await results.json();
+    return dispatch({
+      type: "CITY_DETAILS",
+      cityName: details.network.location.city,
+      cityBike: details.network.name,
+      stations: details.network.stations,
+      longitud: details.network.location.longitude,
+      latitude: details.network.location.latitude
+    });
   };
 }
